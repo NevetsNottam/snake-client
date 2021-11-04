16f3adf76c72f22fba6f0e9c2d72e455759ffee4
steven <stevenmatton100@gmail.com>
2021-11-03 20:10:47 -0400

    fixed my snake

--- play.js
+++ play.js
@@ -3,15 +3,21 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
+    host: "165.227.47.243",  // IP address here,
     port: 50541// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("timeout", () => {
-    console.log("your snake died");
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Hello");
+
+    conn.on('data', (data) => {
+      console.log(data.toString());
+      conn.end();
+    });
   });
 
   return conn;
