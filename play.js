02c8ff07b40065549ce3fb7defb5e245ead6385e
steven <stevenmatton100@gmail.com>
2021-11-03 22:03:59 -0400

    added a user input termination command

--- play.js
+++ play.js
@@ -1,7 +1,25 @@
 const net = require("net");
 const connect = require("./client");
 
-
-
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const handleUserInput = (key) => {
+  // your code here
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput()
